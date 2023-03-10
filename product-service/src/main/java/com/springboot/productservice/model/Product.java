2f93f4af8bb14a9529ea3b0860198f454c419510
Anagha More <moreanagha13@gmail.com>
2023-03-10 17:14:11 +0530

    Integration testing

--- product-service/src/main/java/com/springboot/productservice/model/Product.java
+++ product-service/src/main/java/com/springboot/productservice/model/Product.java
@@ -1,6 +1,4 @@
 package com.springboot.productservice.model;
-
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- product-service/src/test/java/com/springboot/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/springboot/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,75 @@
 package com.springboot.productservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.productservice.dto.ProductRequest;
+import com.springboot.productservice.repository.ProductRepository;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer(new DockerImageName("mongo: 4.4.2"));
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;//convert the json to string ... seralized and deserialzed the request and response
+	@Autowired
+	private ProductRepository productRepository;
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri",mongoDBContainer::getReplicaSetUrl);
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateproduct() throws Exception {
+		// mock mvc object to make a request to product controller
+
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest); // to convert the json response into string make us eof objectwrapper
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString))
+				.andExpect(status().isCreated());
+		Assertions.assertTrue(productRepository.findAll().size() == 1);
+
+	}
+
+	private ProductRequest getProductRequest(){
+		return ProductRequest.builder()
+				.name("iphone 13")
+				.description("iphone 13")
+				.price(BigDecimal.valueOf(1200))
+				.build();
 	}
 
 }
+
+
+// ACT AS A MONGODB DATABASE and need to set a uri to access this db
+// firstly it eill start the container by downloading mongo given version and it will dynamically assign the
+//replica set url to spring
+// need to create dynamic url
